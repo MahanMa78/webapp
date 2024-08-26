dc8bdf56131e69f6f0d3ba997fc9935c8cf7746a
Mahan Mazaheri <mahan78ir@gmail.com>
2024-08-26 19:09:11 +0330

    add order field to Tag model

--- posts/models.py
+++ posts/models.py
@@ -22,6 +22,10 @@ class Post(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=20)
     slug = models.SlugField(max_length=20 , unique=True)
-    
+    order = models.IntegerField(null=True)
+        
     def __str__(self):
         return self.name
+    
+    class Meta:
+        ordering = ['order']
