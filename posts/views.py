608575ed284a0b1b6c5172e2401bc7a7cd6dc30c
Mahan Mazaheri <mahan78ir@gmail.com>
2024-09-04 18:47:56 +0330

    add htmx for reply and comment

--- posts/views.py
+++ posts/views.py
@@ -127,6 +127,7 @@ def post_page_view(request , pk):
 @login_required
 def comment_sent(request , pk):
     post = get_object_or_404(Post , id = pk)
+    replyform = ReplyCreateForm()
     
     if request.method == 'POST':
         form = CommentCreateForm(request.POST)
@@ -136,12 +137,19 @@ def comment_sent(request , pk):
             comment.parent_post = post
             comment.save()
             
-    return render(request , 'posts/comment.html' , {"comment" : comment})
+    context = {
+         "post" : post,
+         "comment" : comment ,
+         'replyform': replyform,
+    }
+            
+    return render(request , 'snippets/add_comment.html' , context)
 
 
 @login_required
 def reply_sent(request , pk):
     comment = get_object_or_404(Comment , id = pk)
+    replyform = ReplyCreateForm()
     
     if request.method == 'POST':
         form = ReplyCreateForm(request.POST)
@@ -151,7 +159,13 @@ def reply_sent(request , pk):
             reply.parent_comment = comment
             reply.save()
             
-    return redirect('post' , comment.parent_post.id)
+    context = {
+         "reply" : reply,
+         "comment" : comment ,
+         'replyform': replyform,
+    }
+            
+    return render(request , 'snippets/add_reply.html' , context)
 
 
 @login_required
